454aa4d1bb19ac37f9276f0e4c669fa91b730725
Muhammad Anas <113907269+SheikhChilli786@users.noreply.github.com>
2024-10-27 21:23:13 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ import asyncio
 from datetime import datetime
 import threading
 from functools import wraps
-import os
+import os=
+import sys
 from dotenv import load_dotenv  # Import load_dotenv to load .env files
 load_dotenv()
 
@@ -15,7 +16,12 @@ if hasattr(asyncio, 'WindowsSelectorEventLoopPolicy'):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 # Logging configuration
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,  # Set the logging level
+    handlers=[
+        logging.StreamHandler(sys.stdout),  # Output logs to stdout
+    ]
+)
 
 # API Credentials and Exchange Configuration
 API_KEY = os.environ.get('API_KEY')
